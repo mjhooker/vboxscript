a4164d5a4e72e7eaa2c7f62138536d999d37ccba
mjhooker <mjhooker@mjhooker-laptop.(none)>
2011-02-25 09:04:21 +0000

    add extra options, split options into multiple lines

--- buildvm.sh
+++ buildvm.sh
@@ -1,10 +1,38 @@
 #!/bin/bash
 
 vboxmanage createvm -name $1 --register
-vboxmanage modifyvm $1 --synthcpu on --ostype WindowsXP --memory 512 --nic1 bridged --bridgeadapter1 eth0 --acpi off --pae off
-vboxmanage storagectl $1 --name SATA0 --add sata
+vboxmanage modifyvm $1 \
+ --synthcpu on \
+ --ostype WindowsXP \
+ --memory 512 \
+ --nic1 bridged \
+ --bridgeadapter1 eth1 \
+ --acpi off \
+ --pae off \
+ --hwvirtex off \
+ --vram 14
 
-vboxmanage createhd --filename $1 --size 10480 --variant Standard
+vboxmanage storagectl $1 \
+--name IDE0 \
+--add ide
+
+vboxmanage createhd \
+--filename $1 \
+--size 10480 \
+--variant Standard
 
 
-vboxmanage storageattach $1 --storagectl SATA0 --port 0 --device 0 --type hdd --medium ${1}.vdi
+vboxmanage storageattach $1 \
+--storagectl IDE0 \
+--port 0 \
+--device 0 \
+--type hdd \
+--medium ${1}.vdi
+
+vboxmanage storageattach $1 \
+--storagectl IDE0 \
+--port 1 \
+--device 0 \
+--type dvddrive \
+--medium emptydrive
+
